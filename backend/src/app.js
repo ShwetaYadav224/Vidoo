7941a593d898fcbdbaa75455d4f707b640a32412
ShwetaYadav224 <shweta22y@gmail.com>
2025-12-11 22:09:33 +0530

    setup-backend

---
+++ backend/src/app.js
@@ -0,0 +1,25 @@
+import "dotenv/config";
+import express from "express";
+import { createServer } from "node:http";
+import { connectToSocket } from "./controllers/socketManager.js";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", process.env.PORT || 3000);
+app.use(cors());
+app.use(express.json({ limit: "70kb" }));
+app.use(express.urlencoded({ extended: true, limit: "70kb" }));
+
+const start = async () => {
+    const connectionDB = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(connectionDB.connection.host);
+    server.listen(app.get("port"), () => {
+        console.log("Server running on port " + app.get("port"));
+    });
+}
+
+start();
---
+++ backend/src/models/meeting.models.js
@@ -0,0 +1,9 @@
+import { Schema } from "mongoose";
+const meetingSchema = new Schema({
+    user_id: { type: String, required: true },
+    meetingCode: { type: String, required: true },
+    meeting_name: { type: String, required: true },
+    date: { type: Date, default: Date.now(), required: true },
+})
+const Meeting = mongoose.model("Meeting", meetingSchema);
+export default Meeting;
